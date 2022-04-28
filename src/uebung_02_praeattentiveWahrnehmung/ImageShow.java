54f585a629170c6296f50bdf8a393ac611430527
Keoma Trippner <TK6993@users.noreply.github.com>
2022-04-28 16:54:01 +0200

    splitted between perception images and info images

--- src/uebung_02_praeattentiveWahrnehmung/ImageShow.java
+++ src/uebung_02_praeattentiveWahrnehmung/ImageShow.java
@@ -10,6 +10,8 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
 public class ImageShow {
 
@@ -21,46 +23,89 @@ public class ImageShow {
 	 */
 	private Display display;
 	private Label label;
+	public  ArrayList<TimedImage> infoImages;
+	public  boolean testactive = false;
+	private int infoImageindex;
+	
+	public  ArrayList<TimedImage> perceptionImages;
 	private int imageindex;
-	public  ArrayList<TimedImage> allImages;
+	private boolean[] perceptionResults;
 	
 	
 	public ImageShow(Display display, Label label) {
 		this.display = display;
 		this.label =label;
-		this.allImages = new ArrayList<TimedImage>();
+		this.perceptionImages = new ArrayList<TimedImage>();
+		this.infoImages = new ArrayList<TimedImage>();
 		this.imageindex = 0;
+		this.infoImageindex =0;
 		createImages(this.display, this.label );
+		perceptionResults = new boolean[perceptionImages.size()-1];
 	}
 	
 	
 	
 	public  void createImages(Display display, Label label) {
-		TimedImage one = new TimedImage(display, label, 500f, "C:\\Users\\keoma\\workspace\\uebung_02_praeattentiveWahrnehmung\\img\\sample.png");
-		TimedImage two = new TimedImage(display, label, 1500f, "C:\\Users\\keoma\\workspace\\uebung_02_praeattentiveWahrnehmung\\img\\visualization.png");
-		TimedImage three = new TimedImage(display, label, 250f, "C:\\Users\\keoma\\workspace\\uebung_02_praeattentiveWahrnehmung\\img\\infographic.png");
+		TimedImage a = new TimedImage(display, label, 1500f, "C:\\Users\\keoma\\workspace\\uebung_02_praeattentiveWahrnehmung\\img\\sample.png");
+		TimedImage b = new TimedImage(display, label, 1000f, "C:\\Users\\keoma\\workspace\\uebung_02_praeattentiveWahrnehmung\\img\\visualization.png");
+		TimedImage c = new TimedImage(display, label, 2500f, "C:\\Users\\keoma\\workspace\\uebung_02_praeattentiveWahrnehmung\\img\\infographic.png");
 
-		allImages.add(one);
-		allImages.add(two);
-		allImages.add(three);
+		
+		
+		
+		TimedImage three = new TimedImage(display, label, -1f, "C:\\Users\\keoma\\workspace\\uebung_02_praeattentiveWahrnehmung\\img\\number-3.png");
+		TimedImage two = new TimedImage(display, label, 700f, "C:\\Users\\keoma\\workspace\\uebung_02_praeattentiveWahrnehmung\\img\\number-2.png");
+		TimedImage one = new TimedImage(display, label, 700f, "C:\\Users\\keoma\\workspace\\uebung_02_praeattentiveWahrnehmung\\img\\number-1.png");
+		TimedImage go = new TimedImage(display, label, 900f, "C:\\Users\\keoma\\workspace\\uebung_02_praeattentiveWahrnehmung\\img\\go.png");
+
+		perceptionImages.add(a);
+		perceptionImages.add(b);
+		perceptionImages.add(c);
+		
+		infoImages.add(three);
+		infoImages.add(two);
+		infoImages.add(one);
+		infoImages.add(go);
+
+
+
 		
 	}
 	
 	public TimedImage getcurrentImage() {
-		 TimedImage toReturn = allImages.get(imageindex);
+		 TimedImage toReturn = perceptionImages.get(imageindex);
 		 return toReturn;
+	}
+	
+	public TimedImage getcurrentInfoImage() {
+		 TimedImage toReturn = infoImages.get(infoImageindex);
+		 return toReturn;
+	}
+	
+	public void setToNextInfoImage() {
+		infoImageindex++;
+		System.out.print(infoImageindex);
+		if(infoImageindex >= infoImages.size()) {
+			infoImageindex= 0;
+			testactive = true;
+		}
+		
 	}
 	
 	public void setToNextImage() {
 		imageindex++;
+		testactive = false;
+
 	}
 	
+	public void setCurrentTest(boolean testresult) {
+		perceptionResults[imageindex] = testresult;
+		System.out.print(testresult);
+	}
 	
 	
 	public static void main(String[] args) {
 		
-		
-
 		
 
 		Display display = Display.getDefault();
@@ -70,30 +115,62 @@ public class ImageShow {
 		
 		Label label = new Label(shell, SWT.NONE);
 		ImageShow program = new ImageShow(display, label);
-		
-		TimedImage one = new TimedImage(display, label, 200f, "C:\\Users\\keoma\\workspace\\uebung_02_praeattentiveWahrnehmung\\img\\sample.png");
-		
+				
 		Button btnSawIt = new Button(shell, SWT.NONE);
+		btnSawIt.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				program.setCurrentTest(true);
+				program.setToNextImage();
+			}
+		});
 		btnSawIt.setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_GREEN));
 		btnSawIt.setBounds(80, 401, 105, 35);
 		btnSawIt.setText("Saw it!");
 		
 		Button btnNoChance = new Button(shell, SWT.NONE);
+		btnNoChance.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				program.setCurrentTest(false);
+				program.setToNextImage();
+			}
+		});
 		btnNoChance.setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_RED));
 		btnNoChance.setBounds(310, 401, 105, 35);
 		btnNoChance.setText("No chance");
 		
+		Button btnNextPerception = new Button(shell, SWT.NONE);
+		btnNextPerception.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				program.setToNextInfoImage();
+			}
+		});
+		btnNextPerception.setBounds(185, 350, 136, 35);
+		btnNextPerception.setText("Next Perception");
 		
-		/*Image image = new Image(display,"C:\\Users\\keoma\\workspace\\uebung_02_praeattentiveWahrnehmung\\img\\sample.png");
-		label.setImage(image);
-		label.setBounds(40, 38, 400, 280);*/
 
 		shell.open();
 		shell.layout();
 		while (!shell.isDisposed()) {
 			
-			boolean currentImageIsShown = program.getcurrentImage().showImage();
-			if(!currentImageIsShown)program.setToNextImage();
+			
+			if(program.testactive) {
+				boolean currentTestImageIsShown = program.getcurrentImage().showImage();
+				if(!currentTestImageIsShown) {
+					program.getcurrentImage().resetImage();
+					program.setToNextImage();
+				}
+			}
+			else {
+				boolean currentInfoImageIsShown = program.getcurrentInfoImage().showImage();
+				if(!currentInfoImageIsShown) {
+					program.getcurrentInfoImage().resetImage();
+					program.setToNextInfoImage();
+				}
+			}
+			
 			
 			 //one.showImage();
 
